0534c4a16d1248ca707dbef87aedaf0fd13eb080
Anzhelika01 <anzi303@abv.bg>
2020-10-06 15:01:04 +0000

    ..

--- src/exercise2/PiAproximation.java
+++ src/exercise2/PiAproximation.java
@@ -4,6 +4,6 @@ package exercise2;
     public static void main(String[] args){
 
       double Pi = 4*(1 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/11 - 1.0/13 + 1.0/17 - 1.0/19 + 1.0/23);
-      System.out.print(Pi);
+      System.out.print("Pi: " + Pi);
     }
   }
